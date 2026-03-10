8baa4320d426c458da5f345d6fffcb03145fd126
MatrikSer <sergeev45223@gmail.com>
2026-03-10 19:26:23 +0500

    3-4

--- index.js
+++ index.js
@@ -4,20 +4,40 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
+        });
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().moveAndHide(block, 5000, {x: 100, y: 20})
+        })
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 5000);
+        })
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            animaster().heartBeating(block, 5000);
+        })
 }
 
 function animaster () {
@@ -36,11 +56,33 @@ function animaster () {
         element.style.transitionDuration =  `${duration}ms`;
         element.style.transform = getTransform(null, ratio);
     }
+    function fadeOut(element, duration) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.remove('show');
+        element.classList.add('hide');
+    }
+    function moveAndHide(element, duration, translation) {
+        move(element, duration * 2/5, translation);
+        fadeOut(element, duration * 3/5);
+    }
+    function showAndHide(element, duration) {
+        fadeIn(element, duration * 1/3);
+        setTimeout(() => _, duration * 1/3);
+        fadeOut(element, duration * 1/3);
+    }
+    function heartBeating(element, duration) {
+        scale(element, duration / 2, 1.4);
+        scale(element, duration / 2, 1);
+    }
 
     const animator = {};
     animator.fadeIn = fadeIn;
     animator.scale = scale;
     animator.move = move;
+    animator.fadeOut = fadeOut;
+    animator.moveAndHide = moveAndHide;
+    animator.heartBeating = heartBeating;
+    animator.showAndHide = showAndHide;
 
     return animator;
 
